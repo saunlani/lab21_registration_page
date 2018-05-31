cf4ecac7e34f057e3b59a5ae65f525c946465910
saunlani <saunlani@gmail.com>
2018-05-31 09:13:46 -0400

    using ORM Entity Framework and SQL

--- lab21_registration_page/Controllers/HomeController.cs
+++ lab21_registration_page/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using lab21_registration_page.Models;
 
 namespace lab21_registration_page.Controllers
 {
@@ -10,51 +11,102 @@ namespace lab21_registration_page.Controllers
     {
         public ActionResult Index()
         {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            ViewBag.Items = ORM.Items.ToList();
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            HttpCookie cookie = Request.Cookies["Counter"];
+            //do they ahve a the cookie called counter?
+            if (cookie == null)
+            {
+                //if no, make a cookie :)
+                cookie = new HttpCookie("Counter");
+                cookie.Value = "0";
+            }
+
+            // increment -- increase the visit count
+            bool success = int.TryParse(cookie.Value, out int visits);
+            if (success)
+            {
+                visits++;
+            }
+            else
+            {
+                visits = 1;
+            }
+            
+            
+            //storing the info in the cookie
+            cookie.Value = visits.ToString();
+            cookie.Expires = DateTime.Now.AddDays(14);
+
+            //return the cookie to the user in the HTTP response
+            Response.Cookies.Add(cookie);
+
+            ViewBag.Message = "Learn about us!";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            if(Session["Counter"] == null)
+            {
+                Session.Add("Counter", 0);
+            }
+            int visits = (int)Session["Counter"];
+            visits++;
+
+            Session["Counter"] = visits;
+
+            ViewBag.Message = "We'd love to hear from you!";
 
             return View();
         }
 
+        public ActionResult Logout ()
+        {
+            Session.Clear();
+            return View();
+        }
         public ActionResult Register()
         {
             ViewBag.Message = "Your Registration page.";
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            ViewBag.Items = ORM.Items.ToList();
+
+            List<Coffee> coffeList = new List<Coffee>();
+            Coffee c1 = new Coffee("Bushy Tail", 10, "Nicaragua");
+            Coffee c2 = new Coffee("Tarrazu", 16, "Costa Rica");
+            Coffee c3 = new Coffee("Don Pablo", 10, "Mexico");
+            coffeList.Add(c1);
+            coffeList.Add(c2);
+            coffeList.Add(c3);
+            ViewBag.CoffeeList = coffeList;
 
             return View();
         }
 
-        public ActionResult Confirmation(string _firstname, string _lastname, 
-            string _birthdate, string _email, string _password, string _phonenumber, 
-            string _gender, string _subscribe, string _fdrink)
+        public ActionResult Confirmation(User data)
         {
-            ViewBag.FirstName = _firstname;
-            ViewBag.LastName = _lastname;
-            ViewBag.Birthdate = _birthdate;
-            ViewBag.Email = _email;
-            ViewBag.Password = _password;
-            ViewBag.PhoneNumber = _phonenumber;
-            ViewBag.Gender = _gender;
-            ViewBag.Subscribe = _subscribe;
-            ViewBag.FavDrink = _fdrink;
-            return View();
-        }
+            //build our ORM Object Relational Mapping
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            if (ModelState.IsValid)
+            {
+                //if the model is valid then we add to our DB
+                ORM.Users.Add(data);
+                //we have to save our changes or they won't stay in our DB
+                ORM.SaveChanges();
+                ViewBag.message = $"{data.FirstName} has been added";
+            }
+            else
+            {
+                ViewBag.message = "Item is not valid, cannot add to DB.";
+            }
 
-        public ActionResult Welcome(string input)
-        {
-            ViewBag.Message = "Your Welcome page.";
-
-            ViewBag.FirstName = input;
             return View();
         }
     }
